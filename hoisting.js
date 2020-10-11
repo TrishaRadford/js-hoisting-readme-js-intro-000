1b649360abe71360ec2126999e0129386c6290c9
convivial-reducer-7231 <tradford10@gmail.com>
2020-10-11 19:51:59 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -10,12 +10,12 @@ function callMe() {
 
 function thisIsCrazy() {
   // fix the code in here:
-
+var crazy = "hey!!!";
   thisIsCrazy();
 
   var thisIsCrazy = function (){
 
-    console.log("hey!!!");
+    console.log(crazy);
   }
 }
 
